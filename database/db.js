c54547ae2b01358a0db8d6b269295377fcb4df36
SMKXT7 <114537328+SMKXT7@users.noreply.github.com>
2022-11-18 21:44:04 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = '';
+var url = 'https://data.mongodb-api.com/app/data-gdzde/endpoint/data/v1';
 try {
 if(url == '') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
@@ -12,7 +12,7 @@ if(url == '') throw console.log(color('Cek konfigurasi database, var url belum d
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, ZhirrrGanss','green'))
+  console.log(color('Connected correctly to server, CANDRA','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
